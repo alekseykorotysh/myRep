2fcef17adfc965a5bb13883e3c867c7dfd7326df
okorotysh <okorotysh@playtika.com>
2019-07-18 19:44:57 +0300

    sdfsdf

--- src/main/java/com/oleksii/controllers/BotMessagesHandler.java
+++ src/main/java/com/oleksii/controllers/BotMessagesHandler.java
@@ -21,35 +21,36 @@ import java.util.List;
 @RequestMapping(path = "/api/messages")
 public class BotMessagesHandler {
 
-  @Autowired
-  private MicrosoftAppCredentials credentials;
+    @Autowired
+    private MicrosoftAppCredentials credentials;
 
-  @Autowired
-  private List<ResourceResponse> responses;
+    @Autowired
+    private List<ResourceResponse> responses;
 
-  @GetMapping(path = "/hi")
-  public String get() {
-    return "Hi";
-  }
+    @GetMapping(path = "/hi")
+    public String get() {
+        return "Hi";
+    }
 
-  @PostMapping(path = "")
-  public List<ResourceResponse> create(@RequestBody @Valid
-  @JsonDeserialize(using = DateTimeDeserializer.class) Activity activity) {
-    ConnectorClient connector =
-        new ConnectorClientImpl(activity.serviceUrl(), credentials);
+    @PostMapping(path = "")
+    public List<ResourceResponse> create(@RequestBody @Valid
+                                         @JsonDeserialize(using = DateTimeDeserializer.class) Activity activity) {
+        System.out.println("AAAAAA" + activity);
+        ConnectorClient connector =
+                new ConnectorClientImpl(activity.serviceUrl(), credentials);
 
-    Activity echoActivity = ActivityCreator.createEchoActivity(activity);
-    Activity checkedActivity = ActivityCreator.createSpellCheckedActivity(activity);
-    Conversations conversation = ConversationCreator.createResponseConversation(connector);
+        Activity echoActivity = ActivityCreator.createEchoActivity(activity);
+        Activity checkedActivity = ActivityCreator.createSpellCheckedActivity(activity);
+        Conversations conversation = ConversationCreator.createResponseConversation(connector);
+        System.out.println("AAAAAA" + conversation);
+        ResourceResponse echoResponse =
+                ResourceResponseSender.send(conversation, activity, echoActivity);
+        responses.add(echoResponse);
 
-    ResourceResponse echoResponse =
-        ResourceResponseSender.send(conversation, activity, echoActivity);
-    responses.add(echoResponse);
-
-    ResourceResponse spellCheckedResponse =
-        ResourceResponseSender.send(conversation, activity, checkedActivity);
-    responses.add(spellCheckedResponse);
-
-    return responses;
-  }
+        ResourceResponse spellCheckedResponse =
+                ResourceResponseSender.send(conversation, activity, checkedActivity);
+        responses.add(spellCheckedResponse);
+        System.out.println("AAAAAA" + responses);
+        return responses;
+    }
 }
