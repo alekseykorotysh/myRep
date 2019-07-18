e1137252274fdb447a50d721b4f054ebb16a6069
okorotysh <okorotysh@playtika.com>
2019-07-18 18:04:48 +0300

    sdf

--- src/main/java/com/oleksii/controllers/BotMessagesHandler.java
+++ src/main/java/com/oleksii/controllers/BotMessagesHandler.java
@@ -1,18 +1,55 @@
 package com.oleksii.controllers;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.joda.deser.DateTimeDeserializer;
+import com.microsoft.bot.connector.ConnectorClient;
+import com.microsoft.bot.connector.Conversations;
+import com.microsoft.bot.connector.customizations.MicrosoftAppCredentials;
+import com.microsoft.bot.connector.implementation.ConnectorClientImpl;
+import com.microsoft.bot.schema.models.Activity;
+import com.microsoft.bot.schema.models.ResourceResponse;
+import com.oleksii.creators.ActivityCreator;
+import com.oleksii.creators.ConversationCreator;
+import com.oleksii.senders.ResourceResponseSender;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/messages")
-@Slf4j
 public class BotMessagesHandler {
 
-  @GetMapping(path = "")
+  @Autowired
+  private MicrosoftAppCredentials credentials;
+
+  @Autowired
+  private List<ResourceResponse> responses;
+
+  @GetMapping(path = "/hi")
   public String get() {
-      log.error("HELLO HELLO");
-   return "Hi";
+    return "Hi";
+  }
+
+  @PostMapping(path = "")
+  public List<ResourceResponse> create(@RequestBody @Valid
+  @JsonDeserialize(using = DateTimeDeserializer.class) Activity activity) {
+    ConnectorClient connector =
+        new ConnectorClientImpl(activity.serviceUrl(), credentials);
+
+    Activity echoActivity = ActivityCreator.createEchoActivity(activity);
+    Activity checkedActivity = ActivityCreator.createSpellCheckedActivity(activity);
+    Conversations conversation = ConversationCreator.createResponseConversation(connector);
+
+    ResourceResponse echoResponse =
+        ResourceResponseSender.send(conversation, activity, echoActivity);
+    responses.add(echoResponse);
+
+    ResourceResponse spellCheckedResponse =
+        ResourceResponseSender.send(conversation, activity, checkedActivity);
+    responses.add(spellCheckedResponse);
+
+    return responses;
   }
 }
