1733b87aa66074fedfc48f2834ecb346415bebe9
okorotysh <okorotysh@playtika.com>
2019-07-18 16:48:20 +0300

    Revert: new copy paste

--- src/main/java/com/oleksii/controllers/BotMessagesHandler.java
+++ src/main/java/com/oleksii/controllers/BotMessagesHandler.java
@@ -1,15 +1,18 @@
 package com.oleksii.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/api/messages")
+@Slf4j
 public class BotMessagesHandler {
 
   @GetMapping(path = "")
   public String get() {
+      log.error("HELLO HELLO");
    return "Hi";
   }
 }
